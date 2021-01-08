ebfb6c0cb88fa868ca0069b25ea3b3c64bd213b9
liuxiaoxiang <liuxiaoxiang@honehe-tech.com>
2021-01-08 15:53:51 +0800

    文件上传
    go.mod创建学习

---
+++ gin_demo/demo/paramquery.go
@@ -0,0 +1,101 @@
+package demo
+
+import (
+	"fmt"
+	"gin_demo/entity"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//获取querystring参数
+//querystring指的是URL中?后面携带的参数
+func GetQueryString(r *gin.Engine) {
+	r.GET("/user/search", func(c *gin.Context) {
+		username := c.DefaultQuery("username", "小王子")
+		//username:=c.Query("username")
+		address := c.Query("address")
+		//输出json结果给调用方
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+	})
+
+	//获取form参数
+	//请求的数据通过form表单来提交
+	r.POST("/user/search2", func(c *gin.Context) {
+		//DefaultPostForm 取不到值时会返回制定的默认值
+		//username := c.DefaultPostForm("username", "小王子")
+		username := c.PostForm("username")
+		address := c.PostForm("address")
+		//输出json结果给调用方
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+
+	})
+
+	//获取path参数
+	r.GET("/user/search3/:username/:address", func(c *gin.Context) {
+		username := c.Param("username")
+		address := c.Param("address")
+		//输出json结果给调用方
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"username": username,
+			"address":  address,
+		})
+	})
+}
+
+//参数绑定
+//为了更方便获取请求参数，可以基于请求的Content-Type 识别强求数据类型并利用反射机制自动提取请求中 QueryString、form表单、JSON、XML等参数到结构体中。
+func ParamBind(r *gin.Engine) {
+
+	//绑定JSON的示例（{"user":"lxx","password":"123456"}）
+	r.POST("loginJson", func(c *gin.Context) {
+		var login entity.Login
+		if err := c.ShouldBind(&login); err == nil {
+			fmt.Printf("login info:%#v\n", login)
+			c.JSON(http.StatusOK, gin.H{
+				"user":     login.User,
+				"password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		}
+	})
+
+	//绑定form表单示例（user=lxx&password=123456）
+	r.POST("loginForm", func(c *gin.Context) {
+		var login entity.Login
+		//ShouldBind()会根据请求的Context-Type自行选择绑定器
+		if err := c.ShouldBind(&login); err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"user":     login.User,
+				"password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(),})
+		}
+	})
+
+	//绑定QueryString示例 （/loginQuery?username=lxx&password=123456）
+	r.GET("/loginQuery", func(c *gin.Context) {
+		var login entity.Login
+		//ShouldBind()会根据请求的Content-Type自行选择绑定器
+		if err := c.ShouldBind(&login); err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"username": login.User,
+				"password": login.Password,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error(),})
+		}
+	})
+}
---
+++ gin_demo/demo/request.go
@@ -0,0 +1,41 @@
+package demo
+
+import (
+	"gin_demo/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func Hello(r *gin.Engine) {
+	//GET：请求方式；/hello:请求路径
+	//当客户端以GET方法请求/hello路径时，会执行后面的匿名函数
+	r.GET("/hello", func(c *gin.Context) {
+		// c.JSON:返回JSON格式的数据
+		c.JSON(200, gin.H{
+			"message": "Hello world!",
+		})
+	})
+
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(middleware.SuccessCode, gin.H{
+			"message": "GET",
+		})
+	})
+
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(middleware.SuccessCode, gin.H{
+			"message": "POST",
+		})
+	})
+
+	r.PUT("/book", func(c *gin.Context) {
+		c.JSON(middleware.SuccessCode, gin.H{
+			"message": "PUT",
+		})
+	})
+
+	r.DELETE("/book", func(c *gin.Context) {
+		c.JSON(middleware.SuccessCode, gin.H{
+			"message": "DELETE",
+		})
+	})
+}
